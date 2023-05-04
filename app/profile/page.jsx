637f276e6770db2c89bb81c68e00f79bf033ca59
adrianhajdin <adrianhajdin00@gmail.com>
2023-05-04 13:34:02 +0200

    ...

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
-import Profile from '@components/profile';
+import Profile from '@components/Profile';
 
 const MyProfile = () => {
   const router = useRouter();
