81fd06338fa8dce9939754f018babc45b7a6cc43
Ross <lalanskyl@gmail.com>
2023-05-31 22:44:46 +0300

    Add a unique identifier to the URL to force a cache-busting reload

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,11 +5,24 @@ import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
     try {
-        await connectToDB()
-
-        const prompts = await Prompt.find({}).populate('creator')
-
-        return new Response(JSON.stringify(prompts), { status: 200 })
+        await connectToDB();
+        const prompts = await Prompt.find().populate({
+          path: "creator"
+        });
+    
+        const response = new Response(JSON.stringify(prompts), {
+          status: 200,
+        });
+    
+        // Add a unique identifier to the URL to force a cache-busting reload
+        const url = new URL(req.url);
+        url.searchParams.set("t", Date.now());
+        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Location", url.toString());
+    
+        return response;
     } catch (error) {
         return new Response("Failed to fetch all prompts", { status: 500 })
     }
