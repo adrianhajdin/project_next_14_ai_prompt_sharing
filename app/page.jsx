cd60d47aa599ff20ee9c88b5c1f76fd3c337f19c
fgirse <fgirse@bluewin.ch>
2023-06-12 10:27:34 +0000

    """

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Team from "@sections/Team";
 const Home = () => (
    <>
   <section className='bg-[url("/assets/images/andalusiaRedRoofs.jpg")]
-   bg-cover bg-non repeat
+   bg-cover bg-no-repeat
    w-full h-[calc(100vh - 30px)] flex-center flex-col'>
     <h1 className='head_text text-center'>
       Wohntraum
