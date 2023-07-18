97daac6cd1535a31e836882974d415b9fed257a2
Eric <eyang701@gmail.com>
2023-07-18 11:54:33 -0700

    VKB gladiator

--- app/_app.jsx
+++ app/_app.jsx
@@ -1,13 +1,33 @@
 // 1. import `NextUIProvider` component
 "use client";
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, createTheme } from '@nextui-org/react';
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }) {
+  const lightTheme = createTheme({
+    type: 'light',
+    theme: {
+      colors: {}, // optional
+    }
+  })
+
+  const darkTheme = createTheme({
+    type: 'dark',
+    theme: {
+      colors: {}, // optional
+    }
+  })
   return (
     // 2. Use at the root of your app
 
-    <NextUIProvider>
+    <NextUIProvider
+      defaultTheme="dark"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className
+      }}>
       <SessionProvider session={session}>
 
         <Component {...pageProps} />
