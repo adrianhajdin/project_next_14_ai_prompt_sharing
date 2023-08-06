f447c60742564a49ff0aea7565a077e6b848ec26
Ehriqhck <eyang701@gmail.com>
2023-08-05 18:07:06 -0600

    typescript start

---
+++ components/ChipTest.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChipTest = () => {
+  return (
+    <div>ChipTest</div>
+  )
+}
+
+export default ChipTest
--- components/TestCard.jsx
+++ components/TestCard.jsx
@@ -1,17 +1,9 @@
-"use client";
+'use client';
 import React from "react";
 import {Chip} from "@nextui-org/react";
-import {Avatar, AvatarGroup, AvatarIcon} from "@nextui-org/react";
 
-export default function TestCard() {
+export default function App() {
   return (
-    <div className="flex gap-3 items-center">
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-      <Avatar name="Junior" />
-      <Avatar src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-      <Avatar name="Jane" />
-      <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-      <Avatar name="Joe" />
-    </div>
+    <Chip>Chip</Chip>
   );
 }
